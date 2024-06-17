d1c234a8fd5bd0c1fb47c0a9d487e19c2f8953f7
joomaio.tinhcv <cvtinh2000@gmail.com>
2024-06-17 17:53:09 +0700

    create new Authentication

---
+++ php_modules/demo_auth/demo_session_base/registers/Authentication.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\demo_auth\demo_session_base\registers;
+
+use SPT\Application\IApp;
+use SPT\Response;
+
+class Authentication
+{
+    public static function addGuard(IApp $app)
+    {
+        
+    }
+}
